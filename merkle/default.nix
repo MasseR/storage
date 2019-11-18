f86863a99d5d2dfefc7e2cb8787b967e75975a9c
Mats Rauhala <mats.rauhala@iki.fi>
2019-11-18 21:26:16 +0200

    Initial commit

---
+++ merkle/default.nix
@@ -0,0 +1,26 @@
+{ lib, haskellPackages }:
+
+let cleanSourceFilter = name: type: let baseName = baseNameOf (toString name); in ! (
+      # Filter out version control software files/directories
+      (baseName == ".git" || type == "directory" && (baseName == ".svn" || baseName == "CVS" || baseName == ".hg")) ||
+      # Filter out editor backup / swap files.
+      lib.hasSuffix "~" baseName ||
+      builtins.match "^\\.sw[a-z]$" baseName != null ||
+      builtins.match "^\\..*\\.sw[a-z]$" baseName != null ||
+
+      # Filter out generates files.
+      lib.hasSuffix ".o" baseName ||
+      lib.hasSuffix ".so" baseName ||
+      baseName == "tags" ||
+      # Filter out nix files
+      lib.hasSuffix ".nix" baseName ||
+      # Filter out nix-build result symlinks
+      (type == "symlink" && lib.hasPrefix "result" baseName) ||
+      # Filter out dist
+      (lib.hasPrefix "dist" baseName)
+  );
+
+
+in
+
+haskellPackages.callCabal2nix "merkle" (lib.cleanSourceWith { src = ./.; filter = cleanSourceFilter; }) {}
---
+++ release.nix
@@ -0,0 +1,42 @@
+with (builtins.fromJSON (builtins.readFile ./nixpkgs.ghc.json));
+
+
+let
+  overrides = pkgs: with pkgs.haskell.lib; {
+    haskell = pkgs.haskell // {
+      packages = pkgs.haskell.packages // {
+        ghc865 = pkgs.haskell.packages.ghc865.override {
+          overrides = self: super: with pkgs.haskell.lib; rec {
+            prelude = super.callPackage ./prelude {};
+            merkle = super.callPackage ./merkle {};
+            storage-core = super.callPackage ./storage-core {};
+            storage-server = super.callPackage ./storage-server {};
+          };
+        };
+      };
+    };
+  };
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    inherit sha256;
+  }) { config.packageOverrides = overrides; config.allowUnfree = true; };
+
+in
+
+rec {
+  inherit (pkgs.haskell.packages.ghc865) prelude merkle storage-core storage-server;
+  shell = pkgs.buildEnv {
+    name = "shell";
+    paths = [];
+    buildInputs = with pkgs; [
+      entr
+      ghcid
+      haskell.packages.ghc865.cabal-install
+      haskell.packages.ghc865.hasktags
+      stylish-haskell
+      hlint
+      binutils
+      (haskell.packages.ghc865.ghcWithHoogle (_: storage-server.buildInputs ++ storage-server.propagatedBuildInputs))
+    ];
+  };
+}
