b16facd7a31dcab8cce1adf54f5dcd8e17b7696e
Mats Rauhala <mats.rauhala@iki.fi>
2019-11-18 21:42:37 +0200

    Basic prelude

--- prelude/src/MyPrelude.hs
+++ prelude/src/MyPrelude.hs
@@ -1,1 +1,27 @@
-module MyPrelude where
+{-# LANGUAGE NoImplicitPrelude #-}
+module MyPrelude
+
+  ( module X
+  , module MyPrelude
+  )
+
+  where
+
+import           Control.Monad.Trans  as X (MonadIO, liftIO)
+import           Data.Text            as X (Text)
+import           Prelude              as X hiding (putStrLn, readFile,
+                                            writeFile)
+
+import qualified Data.ByteString      as B
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.Foldable        as F
+import qualified Data.Text.IO         as T
+
+type ByteString = B.ByteString
+type LByteString = LB.ByteString
+
+catMaybes :: Traversable t => t (Maybe a) -> [a]
+catMaybes = concatMap F.toList
+
+putStrLn :: MonadIO m => Text -> m ()
+putStrLn = liftIO . T.putStrLn
