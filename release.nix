26da54d3f53981ece45e72908f758e8d520932a6
Mats Rauhala <mats.rauhala@iki.fi>
2019-12-18 21:39:37 +0200

    Replace local prelude with 'masse-prelude'

--- release.nix
+++ release.nix
@@ -2,12 +2,18 @@ with (builtins.fromJSON (builtins.readFile ./nixpkgs.ghc.json));
 
 
 let
+  masse-prelude-src = pkgs.fetchFromGitHub {
+    owner = "MasseR";
+    repo = "masse-prelude";
+    rev = "a529b7c7e26d2d30ad5442c9124c88c76d2188f8";
+    sha256 = "0m15yasfgm4j2450d2wcgyjj6y75p1sbbx8dgsi3vkll8ari306p";
+  };
   overrides = pkgs: with pkgs.haskell.lib; {
     haskell = pkgs.haskell // {
       packages = pkgs.haskell.packages // {
         ghc865 = pkgs.haskell.packages.ghc865.override {
           overrides = self: super: with pkgs.haskell.lib; rec {
-            prelude = super.callPackage ./prelude {};
+            masse-prelude = super.callPackage masse-prelude-src {};
             merkle = super.callPackage ./merkle {};
             storage-core = super.callPackage ./storage-core {};
             storage-server = super.callPackage ./storage-server {};
@@ -24,7 +30,7 @@ let
 in
 
 rec {
-  inherit (pkgs.haskell.packages.ghc865) prelude merkle storage-core storage-server;
+  inherit (pkgs.haskell.packages.ghc865) merkle storage-core storage-server;
   shell = pkgs.buildEnv {
     name = "shell";
     paths = [];
@@ -39,7 +45,7 @@ rec {
       (haskell.packages.ghc865.ghcWithHoogle
         (_: builtins.concatMap
           (p: p.buildInputs ++ p.propagatedBuildInputs)
-          [ prelude merkle storage-core storage-server ]))
+          [ merkle storage-core storage-server ]))
     ];
   };
 }
