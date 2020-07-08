b5c6720a722fe954f403f779c3fba80bf7f474a8
Mats Rauhala <mats.rauhala@iki.fi>
2020-07-08 10:37:57 +0300

    Add retry

--- storage-server/src/Storage/Main.hs
+++ storage-server/src/Storage/Main.hs
@@ -14,13 +14,19 @@ import           Storage.Logger         (logInfo, withLogger)
 import           Storage.Metrics        (newMetrics)
 import           Storage.Metrics.Push   (startMetricsPush)
 
+import           Control.Retry
 import           Data.Config            (Config (..), readConfig)
+
 
 import           Control.Lens           (view)
 import           Data.Generics.Product  (field)
 import           System.FilePath        ((</>))
 
 import           System.Environment     (getArgs)
+
+import           Control.Monad.Catch    (Handler (..))
+
+import           Network.HTTP.Client    (HttpException)
 
 import           Database.SQLite.Simple (withConnection)
 
@@ -40,5 +46,8 @@ defaultMain = do
   withEnv config $ \environment ->
     runAppM environment $ do
       logInfo "Starting up the server .."
-      traverse_ startMetricsPush (view (field @"config" . field @"metrics") environment)
+      traverse_ (withRetry . startMetricsPush) (view (field @"config" . field @"metrics") environment)
       server
+  where
+    limitedBackoff = exponentialBackoff 50 <> limitRetries 5
+    withRetry f = recovering limitedBackoff [\_st -> Handler (const @_ @HttpException (pure True))] (\_st -> f)
