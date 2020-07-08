c6e9d388d39f6608f70900b839a10b34dea18200
Mats Rauhala <mats.rauhala@iki.fi>
2020-07-08 10:37:57 +0300

    Add retry

--- storage-server/src/Control/Monad/App.hs
+++ storage-server/src/Control/Monad/App.hs
@@ -13,6 +13,8 @@ import           Storage.Logger      (LogM (..))
 
 import           Storage.Environment (Env)
 
+import Control.Monad.Catch (MonadMask, MonadCatch, MonadThrow)
+
 newtype AppM a = AppM (ReaderT Env IO a)
 
 deriving newtype instance Functor AppM
@@ -21,6 +23,9 @@ deriving newtype instance Monad AppM
 deriving newtype instance MonadIO AppM
 deriving newtype instance MonadReader Env AppM
 deriving newtype instance MonadUnliftIO AppM
+deriving newtype instance MonadMask AppM
+deriving newtype instance MonadCatch AppM
+deriving newtype instance MonadThrow AppM
 deriving via (LogM Env) instance Katip AppM
 deriving via (LogM Env) instance KatipContext AppM
 
--- storage-server/src/Storage/Main.hs
+++ storage-server/src/Storage/Main.hs
@@ -13,12 +13,18 @@ import           Storage.Logger        (logInfo, withLogger)
 import           Storage.Metrics       (newMetrics)
 import           Storage.Metrics.Push  (startMetricsPush)
 
+import           Control.Retry
+
 import           Data.Config           (readConfig)
 
 import           Control.Lens          (view)
 import           Data.Generics.Product (field)
 
 import           System.Environment    (getArgs)
+
+import           Control.Monad.Catch   (Handler (..))
+
+import Network.HTTP.Client (HttpException)
 
 withEnv :: (Env -> IO a) -> IO a
 withEnv f = withLogger $ \loggingEnv -> do
@@ -33,5 +39,8 @@ defaultMain :: IO ()
 defaultMain = withEnv $ \environment ->
   runAppM environment $ do
     logInfo "Starting up the server .."
-    traverse_ startMetricsPush (view (field @"config" . field @"metrics") environment)
+    traverse_ (withRetry . startMetricsPush) (view (field @"config" . field @"metrics") environment)
     server
+  where
+    limitedBackoff = exponentialBackoff 50 <> limitRetries 5
+    withRetry f = recovering limitedBackoff [\_st -> Handler (const @_ @HttpException (pure True))] (\_st -> f)
